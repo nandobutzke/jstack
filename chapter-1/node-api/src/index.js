ad570f0679f62b3a5124c69ee180c8bc8aa4727a
Fernando Butzke <nandoproject209@gmail.com>
2022-07-04 23:53:34 -0300

    Query Params + Iterable

--- chapter-1/node-api/src/index.js
+++ chapter-1/node-api/src/index.js
@@ -2,18 +2,22 @@ const http = require('http');
 
 const routes = require('./routes');
 
+const { URL } = require('url');
+
 const server = http.createServer((request, response) => {
-    console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
+    const parsedUrl = new URL(`http://localhost:3333${request.url}`)
 
     const route = routes.find((routeObj) => (
-        routeObj.endpoint === request.url && routeObj.method === request.method
+        routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
     ))
 
     if (route) {
+        request.query = Object.fromEntries(parsedUrl.searchParams);
+
         route.handler(request, response);
     } else {
         response.writeHead(404, { 'Content-type': 'text/html' });
-        response.end(`Cannot ${request.method} ${request.url}`);
+        response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
     }
 });
 
