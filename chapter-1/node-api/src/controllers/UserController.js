8687e018c46244a61485020005c16d6cf9eab7ae
Fernando Butzke <nandoproject209@gmail.com>
2022-07-06 23:59:41 -0300

    Adding createUser function + bodyParser helper

--- chapter-1/node-api/src/controllers/UserController.js
+++ chapter-1/node-api/src/controllers/UserController.js
@@ -12,8 +12,7 @@ module.exports = {
              return a.id > b.id ? 1 : -1
         })
 
-        response.writeHead(200, { 'Content-type': 'application/json' });
-        response.end(JSON.stringify(sortedUsers));
+        response.send(200, sortedUsers);
     },
     
     getUserById(request, response) {
@@ -22,12 +21,23 @@ module.exports = {
         const user = users.find((user) => user.id === Number(id));
 
         if (!user) {
-            response.writeHead(400, { 'Content-type': 'application/json' });
-            response.end(JSON.stringify({ error: "User not found" }));
-        } else {
-            response.writeHead(200, { 'Content-type': 'application/json' });
-            response.end(JSON.stringify(user));
+            return response.send(400, { error: "User not found" })
         }
+        
+        response.send(200, user);
 
+    },
+    createUser(request, response) {
+        const { body } = request;
+
+        const lastUserId = users[users.length - 1].id;
+
+        const newUser = {
+            id: lastUserId + 1,
+            name: body.name
+        }
+        users.push(newUser)
+
+        response.send(200, newUser)
     }
 };
--- chapter-1/node-api/src/index.js
+++ chapter-1/node-api/src/index.js
@@ -4,6 +4,8 @@ const routes = require('./routes');
 
 const { URL } = require('url');
 
+const bodyParser = require('./helpers/bodyParser');
+
 const server = http.createServer((request, response) => {
     const parsedUrl = new URL(`http://localhost:3333${request.url}`)
 
@@ -11,8 +13,6 @@ const server = http.createServer((request, response) => {
     let id = null;
 
     const splitEndpoint = pathname.split('/').filter(Boolean);
-
-    console.log(splitEndpoint);
 
     if (splitEndpoint.length > 1) {
         pathname = `/${splitEndpoint[0]}/:id`;
@@ -27,7 +27,16 @@ const server = http.createServer((request, response) => {
         request.query = Object.fromEntries(parsedUrl.searchParams);
         request.params = { id };
 
-        route.handler(request, response);
+        response.send = (statusCode, body) => {
+            response.writeHead(statusCode, { 'Content-type': 'application/json' });
+            response.end(JSON.stringify(body));
+        }
+
+        if (['POST', 'PUT', 'PATCH'].includes(request.method)) {
+            bodyParser(request, () => route.handler(request, response));
+        } else {
+            route.handler(request, response);
+        }
     } else {
         response.writeHead(404, { 'Content-type': 'text/html' });
         response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
