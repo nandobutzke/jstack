48deff5f0e6e60c3c0bf3290bb4d056040bdbefc
Fernando Butzke <nandoproject209@gmail.com>
2022-07-14 20:38:00 -0300

    Refactoring update and delete method

--- chapter-1/mycontacts-api/src/app/controllers/ContactController.js
+++ chapter-1/mycontacts-api/src/app/controllers/ContactController.js
@@ -2,7 +2,9 @@ const ContactRepository = require('../repositories/ContactRepository');
 
 class ContactController {
   async index(request, response) {
-    const contacts = await ContactRepository.findAll();
+    const { orderBy } = request.query;
+
+    const contacts = await ContactRepository.findAll(orderBy);
 
     response.json(contacts);
   }
@@ -21,12 +23,6 @@ class ContactController {
 
   async delete(request, response) {
     const { id } = request.params;
-
-    const contact = await ContactRepository.findById(id);
-
-    if (!contact) {
-      return response.status(404).json({ error: 'Contact not found' });
-    }
 
     await ContactRepository.delete(id);
     response.sendStatus(204);
