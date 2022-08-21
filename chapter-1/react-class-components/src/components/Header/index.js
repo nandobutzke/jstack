6b96b71b190bbd9444a77ba8a4e1eb33adf3f136
Fernando Butzke <nandoproject209@gmail.com>
2022-08-21 12:56:20 -0300

    feat: Creating a HOC for monitoring theme

--- chapter-1/react-class-components/src/components/Header/index.js
+++ chapter-1/react-class-components/src/components/Header/index.js
@@ -3,19 +3,37 @@ import React, { Component } from 'react';
 import { Container } from './styles';
 import { ThemeContext } from '../../contexts/ThemeContext';
 
-export default class Header extends Component {
+function Hoc(HeaderComponent) {
+  return class Component extends React.Component {
+    render() {
+      return (
+        <ThemeContext.Consumer>
+          {(values) => (
+            <HeaderComponent handleToggleTheme={values.handleToggleTheme} theme={values.theme} />
+          )}
+        </ThemeContext.Consumer>
+      );
+    }
+  }
+}
+
+class Header extends Component {
+  componentDidUpdate(prevProps, _) {
+    if (this.props.theme !== prevProps.theme) {
+      console.log('tema mudou');
+    }
+  }
+
   render() {
     return (
-      <ThemeContext.Consumer>
-        {(values) => (
-          <Container>
-            <h1>JStack's Blog</h1>
-            <button type="button" onClick={values.handleToggleTheme}>
-              { values.theme === 'dark' ? '🌞' : '🌚' }
-            </button>
-          </Container>
-        )}
-      </ThemeContext.Consumer>
+      <Container>
+        <h1>JStack's Blog</h1>
+        <button type="button" onClick={this.props.handleToggleTheme}>
+          { this.props.theme === 'dark' ? '🌞' : '🌚' }
+        </button>
+      </Container>
     );
   }
 }
+
+export default Hoc(Header);
